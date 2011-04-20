2cfdf667ef8c6317dc40f50c51c5943d45ceb6df
Andreas Madsack <andreas.madsack@ims.uni-stuttgart.de>
2011-04-20 11:21:38 +0200

    uniqify buffer-names

---
+++ uniqify.el
@@ -0,0 +1,7 @@
+;; uniquify buffer-names
+
+;; for example if you open two models.py files
+;; shows buffers as app1/models.py and app2/models.py
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward) 
