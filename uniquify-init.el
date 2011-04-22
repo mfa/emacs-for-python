52ad65c9c111d39a518f381d694ad19ad9a28e73
mfa <andreas@madflex.de>
2011-04-22 17:50:32 +0200

    more options for uniquifiy

--- uniquify-init.el
+++ uniquify-init.el
@@ -5,3 +5,7 @@
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward) 
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
